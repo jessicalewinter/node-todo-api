1d48649ed163e35cbb2d66a775f527525ed50393
Jessica Lewinter <jessicalewinter@gmail.com>
2017-11-06 09:39:43 -0300

    Using object notation with mongodb

--- play/mongodb-connect.js
+++ play/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if(err) {
@@ -17,17 +18,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Jessica',
-        age: 23,
-        location: 'Fortaleza- Brazil',
-    }, (error, result) => {
-        if(error){
-            return console.log('Unable to insert todo', error);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Jessica',
+    //     age: 23,
+    //     location: 'Fortaleza- Brazil',
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert todo', error);
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
-    db.close();
+    // db.close();
 });
 
