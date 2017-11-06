93182170beb52dab37f5354b347580a6fdb03c39
Jessica Lewinter <jessicalewinter@gmail.com>
2017-11-06 19:16:28 -0300

    Validator type with Mongoose

--- server/server.js
+++ server/server.js
@@ -5,34 +5,68 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 20,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'New mongoose file!'
+// var newTodo = new Todo({
+//     text: 'New mongoose file!'
+// });
+
+// newTodo.save().then((result) => {
+//     console.log(`Saved todo ${result}`);
+// }, (error) => {
+//     console.log('Unable to save todo');
+// });
+
+// var otherTodo = new Todo({
+//     text: 'Something to do'
+// });
+
+// otherTodo.save().then((result) => {
+//     console.log(JSON.stringify(result, undefined, 2));
+// }, (error) => {
+//     console.log('Unable to save todo');
+// })
+
+
+//User
+//email 
+
+var Users = mongoose.model('Users', {
+    username: {
+        type: String
+    },
+    password:{
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
 });
 
-newTodo.save().then((result) => {
-    console.log(`Saved todo ${result}`);
-}, (error) => {
-    console.log('Unable to save todo');
+var user =  new Users({
+    username: 'jessicalewinter',
+    email: 'jessica.lewinter@gdeste.ifce.edu.br'
 });
 
-var otherTodo = new Todo({
-    text: 'Second Mongoose file',
-    completed: true,
-    completedAt: 123
-});
-
-otherTodo.save().then((result) => {
+user.save().then((result) => {
     console.log(JSON.stringify(result, undefined, 2));
-}, (error) => {
-    console.log('Unable to save todo');
-})
+},(error) => {
+    console.log('Unable to save Todo');
+});
