66c161ad0faaa07ecf1266ae1d04ece87fc73aab
Jessica Lewinter <jessicalewinter@gmail.com>
2017-11-09 20:31:34 -0300

    Testing GET

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect =  require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First text todo'
 },{
+    _id: new ObjectID(),
     text: 'Second text todo'
 }];
 
@@ -68,7 +71,35 @@ describe('GET /todos', () => {
         })
         .end(done);
     });
-})
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return 400 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${hexID}`)
+        .expect(400)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    });
+});
+
 
 
 
